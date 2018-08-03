9773c923da842f354fa2afd21aef0e5144967339
YulianaYershova <juliana.ershova97@gmail.com>
2018-08-03 10:14:40 +0300

    Create BaseFactory

---
+++ src/Factory/BaseFactory.java
@@ -0,0 +1,11 @@
+package Factory;
+
+/**
+ * Created by Julia on 03.08.2018
+ */
+public abstract class BaseFactory {
+
+    public abstract Circle getCircle();
+
+    public abstract Triangle getTriangle();
+}
