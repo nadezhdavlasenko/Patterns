a1355318901eae2b2a2f5e002c1e38aa1bc0cc8c
Anna <annabalaka7@gmail.com>
2018-08-03 10:12:56 +0300

    WhiteFactory

---
+++ src/WhiteFactory.java
@@ -0,0 +1,15 @@
+public class WhiteFactory extends BaseFactory {
+    public WhiteFactory() {
+
+    }
+
+    @Override
+    public Triangle createTriangle(Point x, Point y, Point z) {
+        return new WhiteTriangle(x, y, z);
+    }
+
+    @Override
+    public Circle createCircle(Point o, double r) {
+        return new WhiteCircle(o, r);
+    }
+}
