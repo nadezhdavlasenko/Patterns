014df1b3c4cfc4aede744cd9077170ee05f23f94
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-08-04 10:10:17 +0300

    Create Black and White Circle, modify Circle constructor

---
+++ src/patterns/shapes/circles/BlackCircle.java
@@ -0,0 +1,10 @@
+package patterns.shapes.circles;
+
+import patterns.point.Point;
+
+public class BlackCircle extends Circle {
+
+    public BlackCircle(Point o, double r) {
+        super(o,r);
+    }
+}
---
+++ src/patterns/shapes/circles/WhiteCircle.java
@@ -0,0 +1,10 @@
+package patterns.shapes.circles;
+
+import patterns.point.Point;
+
+public class WhiteCircle extends Circle {
+
+    public WhiteCircle(Point o, double r) {
+        super(o,r);
+    }
+}
