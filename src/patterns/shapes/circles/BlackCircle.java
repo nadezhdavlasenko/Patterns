68a0ec9c96ee58334c3cb06a5451b4a1d08853b0
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-08-04 10:35:07 +0300

    Black and White Circle constructor

--- src/patterns/shapes/circles/BlackCircle.java
+++ src/patterns/shapes/circles/BlackCircle.java
@@ -1,10 +1,12 @@
 package patterns.shapes.circles;
 
 import patterns.point.Point;
+import patterns.shapes.Color;
 
 public class BlackCircle extends Circle {
 
     public BlackCircle(Point o, double r) {
         super(o,r);
+        setColor(Color.BLACK);
     }
 }
--- src/patterns/shapes/circles/WhiteCircle.java
+++ src/patterns/shapes/circles/WhiteCircle.java
@@ -1,10 +1,12 @@
 package patterns.shapes.circles;
 
 import patterns.point.Point;
+import patterns.shapes.Color;
 
 public class WhiteCircle extends Circle {
 
     public WhiteCircle(Point o, double r) {
         super(o,r);
+        setColor(Color.WHITE);
     }
 }
