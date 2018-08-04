2ccd476cf4ad45a29d3779e7d8bd0dd08c5dff90
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-08-04 14:10:08 +0300

    White and Black Circle equals and hashcode

--- src/patterns/shapes/circles/BlackCircle.java
+++ src/patterns/shapes/circles/BlackCircle.java
@@ -3,10 +3,35 @@ package patterns.shapes.circles;
 import patterns.point.Point;
 import patterns.shapes.Color;
 
+import java.util.Objects;
+
 public class BlackCircle extends Circle {
 
+    private Color color = Color.BLACK;
+
     public BlackCircle(Point o, double r) {
-        super(o,r);
-        setColor(Color.BLACK);
+        super(o, r);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BlackCircle that = (BlackCircle) o;
+        return getCenter() == that.getCenter() && getR() == that.getR();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCenter(), getR());
+    }
+
+    @Override
+    public String toString() {
+        return getCenter().toString() + " r = " + getR() + " Color = " + getColor();
     }
 }
--- src/patterns/shapes/circles/WhiteCircle.java
+++ src/patterns/shapes/circles/WhiteCircle.java
@@ -3,10 +3,36 @@ package patterns.shapes.circles;
 import patterns.point.Point;
 import patterns.shapes.Color;
 
+import java.util.Objects;
+
 public class WhiteCircle extends Circle {
 
+    private Color color = Color.WHITE;
+
     public WhiteCircle(Point o, double r) {
-        super(o,r);
-        setColor(Color.WHITE);
+        super(o, r);
     }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof WhiteCircle)) return false;
+        WhiteCircle that = (WhiteCircle) o;
+        return getCenter() == that.getCenter() && getR() == that.getR();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCenter(), getR());
+    }
+
+    @Override
+    public String toString() {
+        return getCenter().toString() + " r = " + getR() + " Color = " + getColor();
+    }
+
 }
