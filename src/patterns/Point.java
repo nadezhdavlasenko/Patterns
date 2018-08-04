2f953f9cc8776d65e1450e01d403f8029918357f
Orest <dorest28@gmail.com>
2018-08-04 08:19:34 +0300

    class Point and Corcle added

---
+++ src/patterns/Point.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "x = " + getX() + " y = " + getY();
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (17 + getX() * 31 + getY() * 17);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point)) return false;
+        Point point = (Point) o;
+        return Double.compare(point.getX(), getX()) == 0 &&
+                Double.compare(point.getY(), getY()) == 0;
+    }
+}
